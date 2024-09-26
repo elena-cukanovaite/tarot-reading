27702caaf47b6d5ec4a3a382c3b7607d23536a87
elena-cukanovaite <ecukanovait1@gmail.com>
2024-09-26 16:05:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './tarot_information.json';
+
+
+const readCSV = () => {
+  console.log(data);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={readCSV}>Generate My Reading</button>
+    </>
   );
 }
 
