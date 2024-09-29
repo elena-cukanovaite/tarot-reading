4c3dfc3e426f499c1a5bf0d5472442286d565bd7
elena-cukanovaite <ecukanovait1@gmail.com>
2024-09-29 11:57:13 +0100

    Resized images for faster loading

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import cardData from './tarot_information.json';
 import React, {useState, useEffect} from 'react';
 
+
+
 function App() {
+  let imagesPath = "./tarot_images/"
+  let imagesFileType = ".jpg"
   const [card, setCard] = useState("M0");
-  const [imagePath, setImagePath] = useState("./"+card+".jpg");
+  const [imagePath, setImagePath] = useState(imagesPath+card+imagesFileType);
 
   const selectCard = () => {
     let cardIds = Object.keys(cardData);
     let item = cardIds[Math.floor(Math.random()*cardIds.length)]
     setCard(item);
-    setImagePath("./"+item+".jpg")
+    setImagePath(imagesPath+item+imagesFileType)
   }
 
   return (
