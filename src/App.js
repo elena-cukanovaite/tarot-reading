3312a773a895cd38d65140c4f3d0c1a948351128
elena-cukanovaite <ecukanovait1@gmail.com>
2024-09-30 22:01:13 +0100

    Added card meanings.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import React, {useState, useEffect} from 'react';
 
 let cardIds = Object.keys(cardData);
 let imageAngles = [{transform:"rotate(0deg)"}, {transform:"rotate(180deg)"}];
-let nameSuffix = ["", " reversed"]
+let nameSuffix = ["", " reversed"];
+let theAnswerUprightOrReversed = ["upright", "reverse"];
 
 function App() {
   let imagesPath = "./tarot_images/"
@@ -14,25 +15,31 @@ function App() {
   const [imagePath, setImagePath] = useState(imagesPath+card+imagesFileType);
   const [reversed, setReversed] = useState({transform:"rotate(0deg)"});
   const [cardName, setCardName] = useState("The Fool");
+  const [theAnswer, setTheAnswer] = useState("");
 
   const selectCard = () => {
     let item = cardIds[Math.floor(Math.random()*cardIds.length)];
     let reversedId = Math.floor(Math.random()*imageAngles.length);
     let reversedOrNot = imageAngles[reversedId];
     let selectedCardName = cardData[item].name;
-  
+    let selectedAnswer = cardData[item][theAnswerUprightOrReversed[reversedId]]["general"];
+
     setCard(item);
     setCardName(selectedCardName + nameSuffix[reversedId]);
     setImagePath(imagesPath+item+imagesFileType);
     setReversed(reversedOrNot);
+    setTheAnswer(selectedAnswer);
   }
 
   return (
     <>
+      <center>
       <button onClick={selectCard}>Generate My Reading</button>
       <br></br>
       <h1>{cardName}</h1>
       <img src={imagePath} style={reversed}/>
+      <p>{theAnswer}</p>
+      </center>
     </>
   );
 }
